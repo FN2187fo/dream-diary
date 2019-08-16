183b180ba2c8d58ba8ae275d7b6be70b0de2e9da
Jasun Chakal <sanjay29@sapura.de>
2019-08-16 17:56:31 +0200

    Swipe to delete in timeline

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -33,6 +33,18 @@ class _TimelineState extends State<Timeline> {
     print(fileContent);
   }
 
+  void deleteContent(index) {
+    Map<String, dynamic> jsonFileContent = jsonDecode(jsonFile.readAsStringSync());
+    print("Json decoded");
+    jsonFileContent.remove(index);
+    print("Removed from fileContent");
+    jsonFile.writeAsStringSync(jsonEncode(jsonFileContent));
+    print("Written to jsonFile");
+    setState(() {
+      fileContent = jsonDecode(jsonFile.readAsStringSync());
+    });
+  }
+
    @override
    Widget build(BuildContext context) {
      return new Scaffold(
@@ -43,10 +55,23 @@ class _TimelineState extends State<Timeline> {
        body: ListView.builder(
          itemCount: fileContent.values.length,
          itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(fileContent.values.elementAt(fileContent.length.toInt() - index.toInt() - 1)), 
-              subtitle: Text(fileContent.keys.elementAt(fileContent.length.toInt() - index.toInt() - 1).split(" ").elementAt(0)),
+          int _index = fileContent.length.toInt() - index.toInt() - 1;
+          return Dismissible(
+            key: Key(fileContent.keys.elementAt(_index)),
+            onDismissed: (direction) {
+              Map<String, dynamic> jsonFileContent = jsonDecode(jsonFile.readAsStringSync());
+              jsonFileContent.remove(fileContent.keys.elementAt(_index));
+              jsonFile.writeAsStringSync(jsonEncode(jsonFileContent));
+              setState(() {
+                fileContent = jsonDecode(jsonFile.readAsStringSync());
+              });
+              Scaffold.of(context).showSnackBar(SnackBar(content: Text("Deleted entry"),));
+            },
+            child: Card(
+              child: ListTile(
+                title: Text(fileContent.values.elementAt(_index)), 
+                subtitle: Text(fileContent.keys.elementAt(_index).split(" ").elementAt(0)),
+              ),
             ),
           );
          },
